4ca2a37468fc923af4b0ee1d3f9878fe00f2c632
Anant Bhushan Batra <anantbhushanbatra@Anants-MacBook-Air.local>
2019-04-08 05:40:31 +0100

    edit firm ui fixed

--- edit_firm.php
+++ edit_firm.php
@@ -23,7 +23,6 @@ echo('
     <tr>
         <td>Field</td>
         <td>Old Value</td>
-        <td>Enter New Value</td>
     </tr>
     <tr>
         <td>provider_id</td>
@@ -32,43 +31,35 @@ echo('
     </tr>
     <tr>
         <td>provider_name</td>
-        <td>'.$firm["provider_name"].'</td>
-        <td><input type="text" name="provider_name"></p></td>
+        <td><input type="text" name="provider_name" value = "'.$firm["provider_name"].'"></p></td>
     </tr>
     <tr>
         <td>provider_email</td>
-        <td>'.$firm["provider_email"].'</td>
-        <td><input type="text" name="provider_email"></p></td>
+        <td><input type="text" name="provider_email" value = "'.$firm["provider_email"].'"></p></td>
     </tr>
     <tr>
         <td>firm_contact_num</td>
-        <td>'.$firm["firm_contact_num"].'</td>
-        <td><input type="text" name="firm_contact_num"></p></td>
+        <td><input type="text" name="firm_contact_num" value = "'.$firm["firm_contact_num"].'"></p></td>
     </tr>
     <tr>
         <td>firm_address</td>
-        <td>'.$firm["firm_address"].'</td>
-        <td><input type="text" name="firm_address"></p></td>
+        <td><input type="text" name="firm_address" value = "'.$firm["firm_address"].'"></p></td>
     </tr>
     <tr>
         <td>green_rate</td>
-        <td>'.$firm["green"].'</td>
-        <td><input type="text" name="green"></p></td>
+        <td><input type="text" name="green" value = "'.$firm["green"].'"></p></td>
     </tr>
     <tr>
         <td>brown_rate</td>
-        <td>'.$firm["brown"].'</td>
-        <td><input type="text" name="brown"></p></td>
+        <td><input type="text" name="brown" value = "'.$firm["brown"].'"></p></td>
     </tr>
     <tr>
         <td>red_rate</td>
-        <td>'.$firm["red"].'</td>
-        <td><input type="text" name="red"></p></td>
+        <td><input type="text" name="red" value = "'.$firm["red"].'"></p></td>
     </tr>
     <tr>
         <td>paymentInfo</td>
-        <td>'.$firm["paymentinfo"].'</td>
-        <td><input type="text" name="paymentinfo"></td>
+        <td><input type="text" name="paymentinfo" value = "'.$firm["paymentinfo"].'"></td>
     </tr>
     <tr>
         <td>account_status</td>
@@ -76,8 +67,7 @@ echo('
     </tr>
     <tr>
         <td>account_comments</td>
-        <td>'.$firm["account_comments"].'</td>
-        <td><input type="text" name="account_comments"></td>
+        <td><input type="text" name="account_comments" value = "'.$firm["account_comments"].'"></td>
     </tr>
     
     </table>
